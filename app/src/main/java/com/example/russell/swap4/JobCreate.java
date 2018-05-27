2573f6204ea00102fabac6358bdddabdf40718e7
lunaortuscor <russngjh@gmail.com>
2018-05-27 10:20:08 +0800

    added buttons to jobcreate

--- app/src/main/java/com/example/russell/swap4/JobCreate.java
+++ app/src/main/java/com/example/russell/swap4/JobCreate.java
@@ -3,10 +3,18 @@ package com.example.russell.swap4;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class JobCreate extends AppCompatActivity {
     private Button cancel;
     private Button save;
+    private TextView textView1;
+    private ImageButton photoButton;
+    private EditText descriptionText;
+    private EditText languageText;
+    private EditText priceText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
