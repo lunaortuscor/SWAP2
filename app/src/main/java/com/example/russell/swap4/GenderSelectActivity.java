3b50af7791594fa295a2d256ccadbc2aa5e1e453
xiurobert <xiurobert@gmail.com>
2018-05-27 13:36:27 +0800

    do something when the user selects the gender

--- app/src/main/java/com/example/russell/swap4/GenderSelectActivity.java
+++ app/src/main/java/com/example/russell/swap4/GenderSelectActivity.java
@@ -1,4 +1,20 @@
 package com.example.russell.swap4;
 
-public class GenderSelectActivity {
+import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+
+public class GenderSelectActivity extends Activity implements OnItemSelectedListener {
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+        // item is adapterView.getItemAtPosition(pos);
+        // this should be passed to the database anyway idk how rn
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 }
