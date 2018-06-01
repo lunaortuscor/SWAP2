b516fc1c159d9979d21904b41a325e446e9cebc3
lunaortuscor <russngjh@gmail.com>
2018-06-01 13:03:29 +0800

    updated profile

--- app/src/main/java/org/swapproject/swapdroid/ViewProfileActivity.java
+++ app/src/main/java/org/swapproject/swapdroid/ViewProfileActivity.java
@@ -3,11 +3,14 @@ package org.swapproject.swapdroid;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.russell.swap4.R;
+
 
 public class ViewProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
     }
 
 
